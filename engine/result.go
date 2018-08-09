a9f49238ed5df04e4cf51b976d63baf006e57cb3
luobaohui <luobaohui@xiaomi.com>
2018-08-09 10:17:11 +0800

    status ok

--- engine/result.go
+++ engine/result.go
@@ -1,19 +1,23 @@
 package engine
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 // JSON .
 func JSON(c *gin.Context, code int, data interface{}) {
 	r, ok := myEngine.codes[code]
 	if !ok {
-		c.JSON(200, gin.H{
+		c.JSON(http.StatusOK, gin.H{
 			"code": 0,
 			"msg":  "未找到匹配的返回码",
 			"data": nil,
 		})
 		return
 	}
-	c.JSON(200, gin.H{
+	c.JSON(http.StatusOK, gin.H{
 		"code": r.Code,
 		"msg":  r.Msg,
 		"data": data,
